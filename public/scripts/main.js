9506bd9bb6611d2838372489c15d14fb46ec3de0
meszarosdezso <meszarosdezsodev@gmail.com>
2022-03-24 10:02:17 +0100

    dark mode

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -3,10 +3,14 @@
 
 import hljs from "https://cdn.skypack.dev/highlight.js"
 
+const isDarkMode =
+  document.documentElement.getAttribute("data-theme") === "dark"
+
+// Syntax Highlighting
 const hljsStyles = document.createElement("link")
+const theme = isDarkMode ? "github-dark" : "github"
 hljsStyles.rel = "stylesheet"
-hljsStyles.href =
-  "//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.5.0/styles/github.min.css"
+hljsStyles.href = `//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.5.0/styles/${theme}.min.css`
 
 document.querySelector("head")?.prepend(hljsStyles)
 
