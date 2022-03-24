3ac343d169298da4923c328dd1070ebfaaba1572
meszarosdezso <meszarosdezsodev@gmail.com>
2022-03-24 09:42:51 +0100

    fix

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -3,6 +3,13 @@
 
 import hljs from "https://cdn.skypack.dev/highlight.js"
 
+const hljsStyles = document.createElement("link")
+hljsStyles.rel = "stylesheet"
+hljsStyles.href =
+  "//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.5.0/styles/github.min.css"
+
+document.querySelector("head").prepend(hljsStyles)
+
 document.querySelectorAll("pre.codeblock").forEach(el => {
-  hljs.highlightElement(el, { language: "rust" })
+  hljs.highlightElement(el)
 })
